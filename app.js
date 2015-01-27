dcc19740e765b1c4f014b6a7a0e4081dace9a9b7
rrgayhart <rrgayhart@gmail.com>
2015-01-27 14:04:02 -0700

    Adds wunder integration

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
-
 var hbs = require('hbs');
-var api_key = process.env.WU_KEY
+var WunderNodeClient = require("wundernode");
+var apikey = process.env.WU_KEY
+
+var debug = false;
+var wunder = new WunderNodeClient(apikey, debug,  10, 'minute');
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
@@ -11,8 +20,16 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/api', function(req, res) {
-  res.send({zipcode: 80304, weather: 'Hot'});
+app.post('/zipcode', function(req, res) {
+  //var zipCode = req.body.zipCode;
+  var query = req.body.zipCode;
+  wunder.conditions(query, function(err, obj) {
+    if (err){
+      console.log('errors: ' + err);
+      res.end("Error processing query string:" + queryData.query);
+    }
+    res.end(obj);
+  });
 });
 
 app.listen(process.env.PORT || 5000);
